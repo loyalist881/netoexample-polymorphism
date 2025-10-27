87775c71dac84cc2209fbefcfe33cf81ab4e3da5
loyalist881 <sokolovpavel31@gmail.com>
2025-10-27 17:07:36 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import taxes.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("=== Пример 1: Компания на УСН 'Доходы' ===");
+        System.out.println("=== Пример 1: Компания на УСН доходы ===");
         Company company1 = new Company("Альфа", new TaxIncome());
         company1.shiftMoney(100000);
         company1.shiftMoney(-20000);
@@ -12,7 +12,7 @@ public class Main {
         System.out.println();
 
 
-        System.out.println("=== Пример 2: Компания на УСН 'Доходы минус расходы' ===");
+        System.out.println("=== Пример 2: Компания на УСН доходы минус расходы ===");
         Company company2 = new Company("Бета", new TaxIncomeMinusOutcome());
         company2.shiftMoney(100000);
         company2.shiftMoney(-20000);
@@ -20,7 +20,7 @@ public class Main {
         System.out.println();
 
 
-        System.out.println("=== Пример 3: Применение сделок (продажа и расходы) ===");
+        System.out.println("=== Пример 3: Применение сделок ===");
         Deal[] deals = {
                 new Sale("телефона", 50000),
                 new Expenditure("аренды офиса", 15000),
